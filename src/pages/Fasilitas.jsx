16aaa6ea152e8bf9506a4c1c333840057bf19473
RafliErii <raflierii01@gmail.com>
2024-06-10 14:59:51 +0700

    add Fasilitas

--- src/pages/Fasilitas.jsx
+++ src/pages/Fasilitas.jsx
@@ -25,7 +25,7 @@ const Fasilitas = () => {
             whileHover={{ scale: 1.05 }}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: index * 0.2 }}
+            transition={{ duration: 0.5, delay: index * 0 }}
           >
             <img src={image.src} alt={image.alt} className="w-full h-full object-cover" />
             <div className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-4 w-full">
